3109027514721a7764f832ca27c2fe2a5332216f
Klaus H. Hutasuhut <klaus.hutasuhut@gmail.com>
2021-01-06 05:41:55 +0700

    deploy my react app to github pages

--- src/main/App.js
+++ src/main/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-import Routes from './config/Routes';
+import IndexStyles from './assets/theme/IndexStyles';
+import Home from './views/Home';
+import About from './views/About';
+import Education from './views/Education';
+import Project from './views/Project';
+import Experience from './views/Experience';
+import Footer from './components/Footer';
+import './assets/css/responsive.css';
+import './assets/css/ionicons.css';
+import './assets/css/fluidbox.min.css';
 
 class App extends Component {
   render() {
     return(
       <>
-        <Routes/> 
+        <IndexStyles/>
+        <Home/>
+        <About/>
+        <Experience/>
+        <Project/>
+        <Education/>
+        <Footer/>
       </>
     );
   }
--- src/main/views/Home.js
+++ src/main/views/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Pic from '../../assets/img/profile.jpg';
+import Pic from '../assets/img/profile.jpg';
 
 function Contactperson(){
 	return(
